d155d8bd67f7ca3e639737367efcdc6f93426138
Rishikesh Vishwakarma <rishikesh.v@purplle.com>
2021-09-10 00:40:09 +0530

    used dto and added all crud methods

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { TicketService } from './ticket.service';
 import { Ticket } from './schemas/ticket.schema';
+import { FilterTicketDto } from './dto/filter-ticket.dto';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 
 @Controller('tickets')
@@ -9,9 +19,14 @@ import { CreateTicketDto } from './dto/create-ticket.dto';
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
+  @Post()
+  create(@Body() createTicketDto: CreateTicketDto) {
+    return this.ticketService.create(createTicketDto);
+  }
+
   @Get()
-  findAll(): Promise<Ticket[]> {
-    return this.ticketService.findAll();
+  findAll(@Query() filterTicketDto: FilterTicketDto): Promise<Ticket[]> {
+    return this.ticketService.findAll(filterTicketDto);
   }
 
   @Get(':id')
@@ -19,8 +34,13 @@ export class TicketController {
     return this.ticketService.findById(id);
   }
 
-  @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketService.create(createTicketDto);
+  @Put(':id')
+  update(@Param('id') id: string, @Body() createTicketDto: CreateTicketDto) {
+    return this.ticketService.update(id, createTicketDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.ticketService.delete(id);
   }
 }
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateTicketDto } from './dto/create-ticket.dto';
 import { Ticket, TicketDocument } from './schemas/ticket.schema';
+import { FilterTicketDto } from './dto/filter-ticket.dto';
+import { CreateTicketDto } from './dto/create-ticket.dto';
 
 @Injectable()
 export class TicketService {
@@ -11,20 +12,56 @@ export class TicketService {
     private readonly ticketModel: Model<TicketDocument>,
   ) {}
 
-  async findAll(): Promise<Ticket[]> {
-    return this.ticketModel.find().exec();
+  async create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    const { customerName, performanceTitle, performanceTime, ticketPrice } =
+      createTicketDto;
+
+    const ticket = new this.ticketModel({
+      creationDate: Date.now(),
+      customerName,
+      performanceTitle,
+      performanceTime,
+      ticketPrice,
+    });
+    return ticket.save();
+  }
+
+  async findAll(filterTicketDto: FilterTicketDto): Promise<Ticket[]> {
+    const offset = filterTicketDto.offset || 0;
+    const limit = filterTicketDto.limit || 10;
+
+    return this.ticketModel.find().skip(offset).limit(limit).exec();
   }
 
   async findById(id: string): Promise<Ticket> {
-    return this.ticketModel
+    const ticket = await this.ticketModel
       .findOne({
         _id: id,
       })
       .exec();
+
+    if (!ticket) {
+      throw new NotFoundException(`Ticket not found`);
+    }
+
+    return ticket;
   }
 
-  async create(createCatDto: CreateTicketDto): Promise<Ticket> {
-    const createdCat = new this.ticketModel(createCatDto);
-    return createdCat.save();
+  async update(id: string, createCatDto: CreateTicketDto): Promise<void> {
+    const result = await this.ticketModel
+      .updateOne({ _id: id }, createCatDto)
+      .exec();
+
+    if (result.nModified === 0) {
+      throw new NotFoundException(`Ticket not found`);
+    }
+  }
+
+  async delete(id: string): Promise<void> {
+    const result = await this.ticketModel.deleteOne({ _id: id }).exec();
+
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`Ticket not found`);
+    }
   }
 }
